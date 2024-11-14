97de402aa3a4873651078f0bd47c590b8f20649a
ameng <2264917049@qq.com>
2024-11-14 10:44:35 +0800

    f

---
+++ tc-src/build/CMakeFiles/tc_http_server.dir/DependInfo.cmake
@@ -0,0 +1,90 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/root/fileserver2/tc-src/redis/async.c" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/redis/async.c.o"
+  "/root/fileserver2/tc-src/redis/dict.c" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/redis/dict.c.o"
+  "/root/fileserver2/tc-src/redis/hiredis.c" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/redis/hiredis.c.o"
+  "/root/fileserver2/tc-src/redis/net.c" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/redis/net.c.o"
+  "/root/fileserver2/tc-src/redis/read.c" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/redis/read.c.o"
+  "/root/fileserver2/tc-src/redis/sds.c" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/redis/sds.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/root/fileserver2/tc-src/HttpConn.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/HttpConn.cpp.o"
+  "/root/fileserver2/tc-src/api/ApiCommon.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/api/ApiCommon.cpp.o"
+  "/root/fileserver2/tc-src/api/ApiDealfile.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/api/ApiDealfile.cpp.o"
+  "/root/fileserver2/tc-src/api/ApiDealsharefile.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/api/ApiDealsharefile.cpp.o"
+  "/root/fileserver2/tc-src/api/ApiLogin.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/api/ApiLogin.cpp.o"
+  "/root/fileserver2/tc-src/api/ApiMd5.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/api/ApiMd5.cpp.o"
+  "/root/fileserver2/tc-src/api/ApiMyfiles.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/api/ApiMyfiles.cpp.o"
+  "/root/fileserver2/tc-src/api/ApiRegister.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/api/ApiRegister.cpp.o"
+  "/root/fileserver2/tc-src/api/ApiSharefiles.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/api/ApiSharefiles.cpp.o"
+  "/root/fileserver2/tc-src/api/ApiSharepicture.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/api/ApiSharepicture.cpp.o"
+  "/root/fileserver2/tc-src/api/ApiUpload.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/api/ApiUpload.cpp.o"
+  "/root/fileserver2/tc-src/base/BaseSocket.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/base/BaseSocket.cpp.o"
+  "/root/fileserver2/tc-src/base/Common.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/base/Common.cpp.o"
+  "/root/fileserver2/tc-src/base/ConfigFileReader.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/base/ConfigFileReader.cpp.o"
+  "/root/fileserver2/tc-src/base/EventDispatch.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/base/EventDispatch.cpp.o"
+  "/root/fileserver2/tc-src/base/HttpParserWrapper.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/base/HttpParserWrapper.cpp.o"
+  "/root/fileserver2/tc-src/base/Lock.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/base/Lock.cpp.o"
+  "/root/fileserver2/tc-src/base/ThreadPool.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/base/ThreadPool.cpp.o"
+  "/root/fileserver2/tc-src/base/UtilPdu.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/base/UtilPdu.cpp.o"
+  "/root/fileserver2/tc-src/base/http_parser.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/base/http_parser.cpp.o"
+  "/root/fileserver2/tc-src/base/netlib.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/base/netlib.cpp.o"
+  "/root/fileserver2/tc-src/base/util.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/base/util.cpp.o"
+  "/root/fileserver2/tc-src/jsoncpp/json_reader.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/jsoncpp/json_reader.cpp.o"
+  "/root/fileserver2/tc-src/jsoncpp/json_value.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/jsoncpp/json_value.cpp.o"
+  "/root/fileserver2/tc-src/jsoncpp/json_writer.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/jsoncpp/json_writer.cpp.o"
+  "/root/fileserver2/tc-src/log/AsyncLogging.cc" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/log/AsyncLogging.cc.o"
+  "/root/fileserver2/tc-src/log/Date.cc" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/log/Date.cc.o"
+  "/root/fileserver2/tc-src/log/FileUtil.cc" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/log/FileUtil.cc.o"
+  "/root/fileserver2/tc-src/log/LogFile.cc" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/log/LogFile.cc.o"
+  "/root/fileserver2/tc-src/log/LogStream.cc" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/log/LogStream.cc.o"
+  "/root/fileserver2/tc-src/log/Logging.cc" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/log/Logging.cc.o"
+  "/root/fileserver2/tc-src/log/TimeZone.cc" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/log/TimeZone.cc.o"
+  "/root/fileserver2/tc-src/log/Timestamp.cc" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/log/Timestamp.cc.o"
+  "/root/fileserver2/tc-src/main.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/main.cpp.o"
+  "/root/fileserver2/tc-src/mysql/DBPool.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/mysql/DBPool.cpp.o"
+  "/root/fileserver2/tc-src/redis/CachePool.cpp" "/root/fileserver2/tc-src/build/CMakeFiles/tc_http_server.dir/redis/CachePool.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "AC_HAS_CRITICAL"
+  "AC_HAS_DEBUG"
+  "AC_HAS_ERROR"
+  "AC_HAS_INFO"
+  "AC_HAS_WARNING"
+  "LINUX_DAEMON"
+  "TIXML_USE_STL"
+  "_FILE_OFFSET_BITS=64"
+  "_REENTRANT"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/fastdfs"
+  "/usr/include/fastcommon"
+  "/usr/local/include/hiredis"
+  "/usr/include/mysql"
+  "../."
+  ".././include"
+  ".././jsoncpp"
+  ".././mysql"
+  ".././redis"
+  ".././base"
+  ".././api"
+  ".././log"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
